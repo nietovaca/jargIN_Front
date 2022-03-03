98901e9ac8042acdfca6822ee0f6929df2d0ad9e
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-03 16:14:41 -0500

    Updated states; attempting front-back server issues

--- README.md
+++ README.md
@@ -1,77 +1,131 @@
-JargIN - Software Engineering Community for Interview Prep
+-GENERAL NOTES/ TO-DOs
 
-Dependencies:
-#styling Material UI
-npm install @mui/material @emotion/react @emotion/styled
+-Set up full CRUD with interview posts
+-Merge backend/frontend
+    -Test through Heroku
+
+    name: '',
+    company: '',
+    jobTitle: '',
+    stage: '',
+    salary: '',
+    location: '',
+    timeLimit: '',
+    date: '',
+    description: '',
+    language: '',
+    notes: '',
+    difficulty: '',
+    offer: '',
+    solution: ''
+
+// ========== Form Functions ============ //
+
+ const handleNewName = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleNewCompany = (event) => {
+    setCompany(event.target.value)
+  }
+
+  const handleNewJobTitle = (event) => {
+    setJobTitle(event.target.value)
+  }
+
+  const handleNewStage = (event) => {
+    setStage(event.target.value)
+  }
+
+  const handleNewSalary = (event) => {
+    setSalary(event.target.value)
+  }
+
+  const handleNewLocation = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const handleNewTimeLimit = (event) => {
+    setTimeLimit(event.target.value)
+  }
+
+  const handleNewDate = (event) => {
+    setDate(event.target.value)
+  }
+
+  const handleNewDescription = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleNewLanguage = (event) => {
+    setLanguage(event.target.value)
+  }
+
+  const handleNewNotes = (event) => {
+    setNotes(event.target.value)
+  }
+
+  const handleNewDifficulty = (event) => {
+    setDifficulty(event.target.value)
+  }
+
+  const handleNewOffer = (event) => {
+    setOffer(event.target.value)
+  }
+
+  const handleNewSolution = (event) => {
+    setSolution(event.target.value)
+  }
+
+<section>
+        <form onSubmit={handleNewFormSubmit}>
+                          {/* add placeholders? */}
+          <p>Name: </p><input type="text" onChange={handleNewName}/><br/>
+          <p>Company: </p><input type="text" onChange={handleNewCompany}/><br/>
+          <p>Location: </p><input type="text" onChange={handleNewLocation}/><br/>
+          <p>Job Title: </p><input type="text" onChange={handleNewJobTitle}/><br/>
+          <p>Salary: </p><input type="text" onChange={handleNewSalary}/><br/>
+          <p>Date: </p><input type="text" onChange={handleNewDate}/><br/>
+          <p>Stage: </p><input type="text" onChange={handleNewStage}/><br/>
+          <p>Time Limit: </p><input type="text" onChange={handleNewTimeLimit}/><br/>
+          <p>Description: </p><textarea name="post-submit" onChange={handleNewDescription}></textarea><br/>
+          <p>Languages used: </p><input type="text" onChange={handleNewLanguage}/><br/>
+          <p>Notes: </p><textarea name="post-submit" onChange={handleNewNotes}></textarea><br/>
+          <p>Difficulty(on a scale of 1 to 10): </p><input type="text" onChange={handleNewDifficulty}/><br/>
+          <p>Offer: </p><input type="text" onChange={handleNewOffer}/><br/>
+          <p>Solution: </p><textarea name="post-submit" onChange={handleNewSolution}></textarea><br/>
+        </form>
+</section>
+
+<section>
+        <h2>Posts:</h2>
+          {
+            setPosts.map((post) => {
+              return (
+                <div key={post._id}>
+                  <h3>{post.name}</h3>
+                  <h3>{post.company}</h3>
+                  <h3>{post.location}</h3>
+                  <h3>{post.jobTitle}</h3>
+                  <h3>{post.salary}</h3>
+                  <h3>{post.date}</h3>
+                  <h3>{post.stage}</h3>
+                  <h3>{post.timeLimit}</h3>
+                  <h3>{post.description}</h3>
+                  <h3>{post.languages}</h3>
+                  <h3>{post.notes}</h3>
+                  <h3>{post.difficulty}</h3>
+                  <h3>{post.offer}</h3>
+                  
+                  <h3>{post.solution}</h3>{/* hidden */}
+                  <button onClick={ (event) => { handleDelete(post) } }>Delete Post</button>
+                </div>
+              )
+            })
+          }
+      </section>
+
+    
 
 
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -7,118 +7,83 @@ const App = () => {
 
 // =========== States ================= //
 
-  const [name, setName] = useState('')
-  const [company, setCompany] = useState('')
-  const [jobTitle, setJobTitle] = useState('')
-  const [stage, setStage] = useState('')
-  const [salaray, setSalary] = useState('')
-  const [location, setLocation] = useState('')
-  const [timeLimit, setTimeLimit] = useState('')
-  const [date, setDate] = useState('')
-  const [description, setDescription] = useState('') // Question, specific code challenge, etc.
-  const [language, setLanguage] = useState('')
-  const [notes, setNotes] = useState('')
-  const [difficulty, setDifficulty] = useState('')
-  const [offer, setOffer] = useState('')
-  const [solution, setSolution] = useState('')
+  // const [name, setName] = useState('')
+  // const [company, setCompany] = useState('')
+  // const [jobTitle, setJobTitle] = useState('')
+  // const [stage, setStage] = useState('')
+  // const [salaray, setSalary] = useState('')
+  // const [location, setLocation] = useState('')
+  // const [timeLimit, setTimeLimit] = useState('')
+  // const [date, setDate] = useState('')
+  // const [description, setDescription] = useState('') // Question, specific code challenge, etc.
+  // const [language, setLanguage] = useState('')
+  // const [notes, setNotes] = useState('')
+  // const [difficulty, setDifficulty] = useState('')
+  // const [offer, setOffer] = useState('')
+  // const [solution, setSolution] = useState('')
 
   const [posts, setPosts] = useState([])
 
   // alternatively:
   // I am less familiar with this method but I believe we would need to use spreading (...) when calling within our functions and return. This just seems neater and could cut down on a massive state list, and having to change setState everytime.
-  // const [post, setPost] = useState({
-  //   name: '',
-  //   company: '',
-  //   jobTitle: '',
-  //   stage: '',
-  //   salary: '',
-  //   location: '',
-  //   timeLimit: '',
-  //   date: '',
-  //   description: '',
-  //   language: '',
-  //   notes: '',
-  //   difficulty: '',
-  //   offer: '',
-  //   solution: ''
-  // })
 
-// =========== useEffect =========== //
+  const [interview, setInterview] = useState({
+    type: '',
+    user: '',
+    date: '',
+    company: '',
+    jobTitle: '',
+    stage: '',
+    salary: '',
+    location: '',
+    timeLimit: '',
+    question: '',
+    devLanguage: '',
+    userResponse: '',
+    difficulty: '',
+    offer: '',
+    comment: []
+  })
+
+  // =========== useEffect =========== //
 
 useEffect(() => {
-  axios.get('https://jargin.herokuapp.com/').then((res) => {
+  axios.get('http://localhost:3000/interviews').then((res) => {
     setPosts(res.data)
   })
 }, [])
 
-// ========== Form Functions ============ //
-
-  const handleNewName = (event) => {
-    setName(event.target.value)
+  const newInterviewPost = (event) => {
+    setInterview({...interview,[event.target.name]:event.target.value})
   }
+  // Then need to display within the input field:
+    // name = (corresponding key from useState)
+    // value = interview.(corresponding key)
 
-  const handleNewCompany = (event) => {
-    setCompany(event.target.value)
-  }
-
-  const handleNewJobTitle = (event) => {
-    setJobTitle(event.target.value)
-  }
-
-  const handleNewStage = (event) => {
-    setStage(event.target.value)
-  }
-
-  const handleNewSalary = (event) => {
-    setSalary(event.target.value)
-  }
-
-  const handleNewLocation = (event) => {
-    setLocation(event.target.value)
-  }
-
-  const handleNewTimeLimit = (event) => {
-    setTimeLimit(event.target.value)
-  }
-
-  const handleNewDate = (event) => {
-    setDate(event.target.value)
-  }
-
-  const handleNewDescription = (event) => {
-    setDescription(event.target.value)
-  }
-
-  const handleNewLanguage = (event) => {
-    setLanguage(event.target.value)
-  }
-
-  const handleNewNotes = (event) => {
-    setNotes(event.target.value)
-  }
-
-  const handleNewDifficulty = (event) => {
-    setDifficulty(event.target.value)
-  }
-
-  const handleNewOffer = (event) => {
-    setOffer(event.target.value)
-  }
-
-  const handleNewSolution = (event) => {
-    setSolution(event.target.value)
-  }
 
 // =========== Post Function ============ //
 
-const handleNewFormSubmit = (event) => {
+const newFormSubmit = (event) => {
+  console.log(interview.user);
   event.preventDefault()
-  axios.post('https://jargin.herokuapp.com/', {
-
-    // Schema from back-end
-
+  axios.post('http://localhost:3000/interviews', {
+    type: interview.type,
+    user: interview.user,
+    date: interview.date,
+    company: interview.company,
+    jobTitle: interview.jobTitle,
+    stage: interview.stage,
+    salary: interview.salary,
+    location: interview.location,
+    timeLimit: interview.timeLimit,
+    question: interview.question,
+    devLanguage: interview.devLanguage,
+    userResponse: interview.userResponse,
+    difficulty: interview.difficulty,
+    offer: interview.offer,
+    comment: []
   }).then(() => {
-    axios.get('https://jargin.herokuapp.com/').then((res) => {
+    axios.get('http://localhost:3000/interviews').then((res) => {
       setPosts(res.data)
     })
   })
@@ -126,14 +91,14 @@ const handleNewFormSubmit = (event) => {
 
 // =========== Delete Function ============ //
 
-const handleDelete = (postsData) => {
-        // Will this work?? - May need to modify url depending on backend routes
-  axios.delete(`https://jargin.herokuapp.com/${postsData._id}`).then((res) => {
-    axios.get('http://localhost:3000/posts').then((res) => {
-      setPosts(res.data)
-    })
-  })
-}
+// const handleDelete = (postsData) => {
+//         // Will this work?? - May need to modify url depending on backend routes
+//   axios.delete(`http://localhost:3000/posts/${postsData._id}`).then((res) => {
+//     axios.get('http://localhost:3000/posts').then((res) => {
+//       setPosts(res.data)
+//     })
+//   })
+// }
 
 
 
@@ -146,50 +111,11 @@ const handleDelete = (postsData) => {
       </header>
       <h1>JargIN</h1>
       <section>
-        <form onSubmit={handleNewFormSubmit}>
-                          {/* add placeholders? */}
-          <p>Name: </p><input type="text" onChange={handleNewName}/><br/>
-          <p>Company: </p><input type="text" onChange={handleNewCompany}/><br/>
-          <p>Location: </p><input type="text" onChange={handleNewLocation}/><br/>
-          <p>Job Title: </p><input type="text" onChange={handleNewJobTitle}/><br/>
-          <p>Salary: </p><input type="text" onChange={handleNewSalary}/><br/>
-          <p>Date: </p><input type="text" onChange={handleNewDate}/><br/>
-          <p>Stage: </p><input type="text" onChange={handleNewStage}/><br/>
-          <p>Time Limit: </p><input type="text" onChange={handleNewTimeLimit}/><br/>
-          <p>Description: </p><textarea name="post-submit" onChange={handleNewDescription}></textarea><br/>
-          <p>Languages used: </p><input type="text" onChange={handleNewLanguage}/><br/>
-          <p>Notes: </p><textarea name="post-submit" onChange={handleNewNotes}></textarea><br/>
-          <p>Difficulty(on a scale of 1 to 10): </p><input type="text" onChange={handleNewDifficulty}/><br/>
-          <p>Offer: </p><input type="text" onChange={handleNewOffer}/><br/>
-          <p>Solution: </p><textarea name="post-submit" onChange={handleNewSolution}></textarea><br/>
+        <form onSubmit={newFormSubmit}>
+          <p>User: </p><input type="text" name="user" value={interview.user} onChange={newInterviewPost}/><br/>
+
+          <input type="submit" value="Submit Post"/>
         </form>
-      </section>
-      <section>
-        <h2>Posts:</h2>
-          {
-            setPosts.map((post) => {
-              return (
-                <div key={post._id}>
-                  <h3>{post.name}</h3>
-                  <h3>{post.company}</h3>
-                  <h3>{post.location}</h3>
-                  <h3>{post.jobTitle}</h3>
-                  <h3>{post.salary}</h3>
-                  <h3>{post.date}</h3>
-                  <h3>{post.stage}</h3>
-                  <h3>{post.timeLimit}</h3>
-                  <h3>{post.description}</h3>
-                  <h3>{post.languages}</h3>
-                  <h3>{post.notes}</h3>
-                  <h3>{post.difficulty}</h3>
-                  <h3>{post.offer}</h3>
-                  
-                  <h3>{post.solution}</h3>{/* hidden */}
-                  <button onClick={ (event) => { handleDelete(post) } }>Delete Post</button>
-                </div>
-              )
-            })
-          }
       </section>
        
     </>
