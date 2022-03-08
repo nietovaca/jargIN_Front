cf3969ba33c4169989c138be268a0e7516ff6803
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-07 21:07:49 -0500

    Minor changes to answer expansion, etc.:

--- src/App.js
+++ src/App.js
@@ -282,7 +282,7 @@ const ExpandMore = styled((props) => {
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
   marginLeft: 'auto',
   transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
+  duration: theme.transitions.duration.shortest,
   }),
 }));
 
